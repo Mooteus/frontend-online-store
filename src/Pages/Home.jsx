c0ab705bccac1a51aad261d9e11f651c707c1d22
Andre Marquez <andre.marqquez@gmail.com>
2022-04-26 16:55:52 -0300

    feat: Adiciona listagem de categorias(Req 04)

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Home.module.css';
+import { getCategories } from '../services/api';
 
 class Home extends Component {
+  state = {
+    data: [],
+  };
+
+  async componentDidMount() {
+    const data = await getCategories();
+    this.setState({
+      data,
+    });
+  }
+
   render() {
+    const { data } = this.state;
+    console.log('data', data);
     return (
-      <main>
-        <label htmlFor="input">
-          <input id="input" type="text" />
-          <Link to="/cart" data-testid="shopping-cart-button">
-            <img src="https://fav.farm/🛒" alt="Button Carrinho de Compras" />
-          </Link>
-        </label>
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
-      </main>
+      <>
+        <main>
+          <label htmlFor="input">
+            <input id="input" type="text" />
+            <Link to="/cart" data-testid="shopping-cart-button">
+              <img src="https://fav.farm/🛒" alt="Button Carrinho de Compras" />
+            </Link>
+          </label>
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+        </main>
+        <nav>
+          <ul>
+            {data.map((category) => (
+              <li key={ category.id } data-testid="category">
+                {category.name}
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </>
     );
   }
 }
