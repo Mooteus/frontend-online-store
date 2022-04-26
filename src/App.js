170777876bdf358f67152875c1664cbe06d81171
Andre Marquez <andre.marqquez@gmail.com>
2022-04-26 15:44:57 -0300

    feat: Adiciona pagina Home(Req 2)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
-import { getCategories } from './services/api';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import Home from './Pages/Home';
 
 function App() {
-  console.log(getCategories());
-  return <p>Project</p>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Home } />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <main>
+        <input type="text" />
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </main>
+    );
+  }
+}
+
+export default Home;
