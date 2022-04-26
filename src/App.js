d95b47a8288aa19345f3f44da250bbf0ca6c21ab
Andre Marquez <andre.marqquez@gmail.com>
2022-04-26 16:32:32 -0300

    feat: Adiciona Componente Cart(Req 03)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import Home from './Pages/Home';
+import Cart from './Pages/Cart';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route exact path="/cart" component={ Cart } />
         <Route exact path="/" component={ Home } />
       </Switch>
     </BrowserRouter>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Home.module.css';
 
 class Home extends Component {
   render() {
     return (
       <main>
-        <input type="text" />
+        <label htmlFor="input">
+          <input id="input" type="text" />
+          <Link to="/cart" data-testid="shopping-cart-button">
+            <img src="https://fav.farm/🛒" alt="Button Carrinho de Compras" />
+          </Link>
+        </label>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
