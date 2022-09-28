ebe2d893660195ea5181f8e1d0c7822c184e5691
Mateus Marçal <moteus10@gmail.com>
2022-09-27 22:18:50 -0300

    Feat: change background color of globalStyle

--- src/utils/globalStyle.js
+++ src/utils/globalStyle.js
@@ -1,9 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
+import { lightGray } from './colors';
 
 const GlobalStyle = createGlobalStyle`
   body {
     padding: 0;
     margin: 0;
+    background-color: ${lightGray}
   };
 `;
 
